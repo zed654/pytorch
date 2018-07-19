64cc97e48b1dc7fa770f8e8ba2c53fae1657dbb3
chp <zed654@naver.com>
2018-07-19 16:25:18 +0900

    18.07.19

--- CNN.py
+++ CNN.py
@@ -23,6 +23,11 @@ testloader = torch.utils.data.DataLoader(testset, batch_size=4, shuffle=False, n
 classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
 
+
+##############################
+########### Viewer ###########
+##############################
+
 import matplotlib.pylab as plt
 import numpy as np
 
@@ -38,3 +43,90 @@ images, labels = dataiter.next()
 imshow(torchvision.utils.make_grid(images))
 
 print(''.join('%5s' % classes[labels[j]] for j in range(4)))
+
+#####################################
+# Define Couvolution Neural Network #
+#####################################
+
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+
+        self.conv1 = nn.Conv2d(3, 24, 5)
+        self.b1 = nn.BatchNorm2d(24)
+        self.pool = nn.MaxPool2d(2, 2)
+
+        self.conv2 = nn.Conv2d(24, 64, 5)
+        self.b2 = nn.BatchNorm2d(64)
+
+        self.fc1 = nn.Linear(64 * 5 * 5, 240)
+        self.fc2 = nn.Linear(240, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.b1((self.conv1(x)))))
+        x = self.pool(F.relu(self.b2(self.conv2(x))))
+        x = x.view(-1, 64 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+net = Net()
+net
+
+
+
+#############################
+########### optim ###########
+#############################
+
+import torch.optim as optim
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(net.parameters())
+
+for epoch in range(10):
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+
+        inputs, labels = data
+
+        inputs, labels = Variable(inputs), Variable(labels)
+
+        optimizer.zero_grad()
+
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.data[0]
+        if i % 128 == 127:  # print every 2000 mini-batches
+            print('[%d, %5d] loss : %.3f' % (epoch + 1, i + 1, running_loss / 128))
+            running_loss = 0.0
+
+    correct = 0
+    total = 0
+    for data in testloader:
+        images, labels = data
+        outputs = net(Variable(images))
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum()
+
+    print('Accuracy 1000 test images : %d %%' % (100 * correct / total))
+
+print('Finished Training')
+
+torch.save(net.state_dict(), 'CNN.pkl')
+
+
+
+
