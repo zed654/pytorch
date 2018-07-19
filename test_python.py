c919c760d8e5bcdae9bc2d7a7d0d43a9cfe11497
Changhyeon Park <zed654@naver.com>
2018-07-19 17:20:46 +0900

    asus_test_python.py

---
+++ test_python.py
@@ -0,0 +1,12 @@
+import torch
+from torch.autograd import Variable
+import torch.nn as nn
+
+class test():
+    def test_def(self):
+        print("test, what are you doing?")
+
+
+test = test()
+test.test_def()
+
