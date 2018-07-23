41ed6b535ec0e0054da2f26ecb536149e4251800
Changhyeon Park <zed654@naver.com>
2018-07-23 16:36:10 +0900

    2018.07.23

--- asus_test1.py
+++ asus_test1.py
@@ -1,5 +1,5 @@
 import torch
-from torch.autograd import Variable
+#from torch.autograd import Variable
 
 dtype = torch.float
 device = torch.device("cpu")
@@ -7,17 +7,30 @@ bias = 1
 # device = torch.device("cuda:0")
 
 
-x = Variable(torch.randn(1, 4 + bias, device=device, dtype=dtype), requires_grad=False)
-y = Variable(torch.randn(1, 3, device=device, dtype=dtype), requires_grad=False)
+#x = Variable(torch.randn(1, 4 + bias, device=device, dtype=dtype), requires_grad=False)
+#y = Variable(torch.randn(1, 3, device=device, dtype=dtype), requires_grad=False)
 
+x = torch.randn(1, 4 + bias,device=device, dtype=dtype, requires_grad=False)
+y = torch.randn(1, 3, device=device, dtype=dtype, requires_grad=False)
+'''
 w1 = Variable(torch.randn(4 + bias, 8 + bias, device=device, dtype=dtype), requires_grad=True)
 w2 = Variable(torch.randn(8 + bias, 6 + bias, device=device, dtype=dtype), requires_grad=True)
 w3 = Variable(torch.randn(6 + bias, 3, device=device, dtype=dtype), requires_grad=True)
+'''
+w1 = torch.randn(4 + bias, 8 + bias, device=device, dtype=dtype, requires_grad=True)
+w2 = torch.randn(8 + bias, 6 + bias, device=device, dtype=dtype, requires_grad=True)
+w3 = torch.randn(6 + bias, 3, device=device, dtype=dtype, requires_grad=True)
 
 r_l = 0.015
 
 for i in range(500):
+    #with torch.no_grad():
+    #    with torch.enable_grad():
+    #        w1.requires_grad = True
+
+    #torch.set_grad_enabled(True)
     #x[0][4] = 1
+    torch.set_grad_enabled(True)
     f1 = torch.mm(x, w1)
     z1 = torch.clamp(f1, min=0)
     #z1[0][8] = 1
@@ -28,11 +41,13 @@ for i in range(500):
     hypothesis = f3
 
     loss = (hypothesis - y).pow(2).sum()
-
     print(i, loss.item())
-
     loss.backward()
 
+    # 새로운 변수를 생성할 때 생성된 변수의 requires_grad가 False가 된다.
+    #   만일 True가 되길원한다면, with torch.enable_grad():도 넣어주면 된다.
+    #   w1 등의 웨이트를 갱신할 때는 grad를 False해주어야 한다.!!!
+    '''
     with torch.no_grad():
         w1 -= r_l * w1.grad
         w2 -= r_l * w2.grad
@@ -41,3 +56,17 @@ for i in range(500):
         w1.grad.zero_()
         w2.grad.zero_()
         w3.grad.zero_()
+    '''
+    #with torch.no_grad():
+    #    with torch.enable_grad():
+    #with torch.set_grad_enabled(False):
+    torch.set_grad_enabled(False)
+    w1 -= r_l * w1.grad
+    w2 -= r_l * w2.grad
+    w3 -= r_l * w3.grad
+
+    w1.grad.zero_()
+    w2.grad.zero_()
+    w3.grad.zero_()
+
+
--- test_python.py
+++ test_python.py
@@ -1,28 +1,22 @@
-class robot:
-  name = 'robot'
-  age = 0
-  def __init__(self, name, age):
-    print('robot 생성자 호출!')
-    self.name = name
-    self.age = age
-  def __del__(self):
-    print('robot 소멸자 호출!')
-  def info(self):
-    print('나의 이름은', self.name, '입니다!')
-    print('나이는', self.age, '입니다!')
+class A:
+  def __init__(self):
+    print("Class A__init__()")
 
-class strong_robot(robot):
-  weapon = 'gun'
-  def __init__(self, name, age, weapon):
-    print('strong_robot 생성자 호출!')
-    super().__init__(name, age)
-    #self.__init__(name, age)
-    self.weapon = weapon
-  def info(self): #오버라이딩
-    super().info()
-    print(self.weapon, '로 싸웁니다!')
-  #def __del__(self):
-  #    print('strong_robot 소멸 자호출!')
 
-s = strong_robot('hulk', 3, 'sword')
-#s.info()
+class B(A):
+  def __init__(self):
+    print("Class B__init__()")
+    A.__init__(self)
+
+class C(A):
+  def __init__(self):
+    print("Class C__init__()")
+    A.__init__(self)
+
+class D(B, C):
+  def __init__(self):
+    print("Class D__init__()")
+    B.__init__(self)
+    C.__init__(self)
+
+d = D()
