85888f2da0bbd99dc14766cd7817e35ff3030754
Changhyeon Park <zed654@naver.com>
2018-07-26 17:22:38 +0900

    2018.07.26

--- Defining_new_autograd_functions.py
+++ Defining_new_autograd_functions.py
@@ -10,24 +10,24 @@ class MyReLU(torch.autograd.Function):
     """
 
     @staticmethod
-    def forward(ctx, input):
+    def forward(self, input):   # Origin 코드는 self가 ctx였음.
         """
         In the forward pass we receive a Tensor containing the input and return
         a Tensor containing the output. ctx is a context object that can be used
         to stash information for backward computation. You can cache arbitrary
         objects for use in the backward pass using the ctx.save_for_backward method.
         """
-        ctx.save_for_backward(input)
+        self.save_for_backward(input)
         return input.clamp(min=0)
 
     @staticmethod
-    def backward(ctx, grad_output):
+    def backward(self, grad_output):
         """
         In the backward pass we receive a Tensor containing the gradient of the loss
         with respect to the output, and we need to compute the gradient of the loss
         with respect to the input.
         """
-        input, = ctx.saved_tensors
+        input, = self.saved_tensors
         grad_input = grad_output.clone()
         grad_input[input < 0] = 0
         return grad_input
