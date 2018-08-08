6b355d963b3a86bbdfb698233cd511f9a37a1858
Changhyeon Park <zed654@naver.com>
2018-08-08 19:17:24 +0900

    2018.08.08

--- CNN.py
+++ CNN.py
@@ -16,7 +16,7 @@ transform = transforms.Compose(
 
 trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=False, transform=transform)
 
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=10)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=4, shuffle=True, num_workers=10)
 
 testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
 
